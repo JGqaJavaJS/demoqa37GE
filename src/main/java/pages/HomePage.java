5601fd5dc05eaff53cf454c4c1b1ee769ff5ee45
julia <juliiagordin@gmail.com>
2024-04-30 13:44:32 +0300

    upd

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -16,4 +16,9 @@ public class HomePage extends BasePage{
         clickWithJSScroll(btnAlertsFrames, 0, 300);
         return new AlertsWindowsPage(driver);
     }
+
+    public HomePage navigateToHomePage() {
+        driver.navigate().to("https://demoqa.com/");
+        return  this;
+    }
 }
